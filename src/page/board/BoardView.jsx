693ff7584a03ce1382173c51e88ecbd8e0602c13
KwonSanta <KwonSanta@gmail.com>
2024-05-21 15:40:34 +0900

    게시물 삭제 시 모달로 확인

--- src/page/board/BoardView.jsx
+++ src/page/board/BoardView.jsx
@@ -7,8 +7,15 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
   useToast,
 } from "@chakra-ui/react";
 
@@ -19,6 +26,7 @@ export function BoardView() {
   const [board, setBoard] = useState(null);
   const toast = useToast();
   const navigate = useNavigate();
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
   // useEffect 훅은 컴포넌트가 마운트된 후에 실행됩니다.
   useEffect(() => {
@@ -82,10 +90,23 @@ export function BoardView() {
       </Box>
       <Box>
         <Button colorScheme={"purple"}>수정</Button>
-        <Button colorScheme={"red"} onClick={handleClickRemove}>
+        <Button colorScheme={"red"} onClick={onOpen}>
           삭제
         </Button>
       </Box>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader />
+          <ModalBody>삭제하시겠습니까?</ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>취소</Button>
+            <Button onClick={handleClickRemove} colorScheme={"red"}>
+              확인
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
