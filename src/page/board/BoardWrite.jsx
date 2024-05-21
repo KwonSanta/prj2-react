9e00c3038558670612832eea39cc3d22acaf2757
KwonSanta <KwonSanta@gmail.com>
2024-05-21 11:35:59 +0900

    글작성 화면 작업

--- src/page/board/BoardWrite.jsx
+++ src/page/board/BoardWrite.jsx
@@ -1,6 +1,56 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export function BoardWrite() {
-  return <Box>글 작성 화면</Box>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [writer, setWriter] = useState("");
+
+  function handleSaveClick() {
+    axios.post("/api/board/add", {
+      // json 형식의 data 가 넘어감
+      title, // 변수명과 프로퍼티명이 같으면 하나만 작성
+      content,
+      writer,
+    });
+  }
+
+  return (
+    <Box>
+      <Box>글 작성 화면</Box>
+      <Box>
+        <Box>
+          <FormControl>
+            <FormLabel>제목</FormLabel>
+            <Input onChange={(e) => setTitle(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>본문</FormLabel>
+            <Textarea onChange={(e) => setContent(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <FormControl>
+            <FormLabel>작성자</FormLabel>
+            <Input onChange={(e) => setWriter(e.target.value)} />
+          </FormControl>
+        </Box>
+        <Box>
+          <Button onClick={handleSaveClick} colorScheme={"blue"}>
+            저장
+          </Button>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
