0d3f139eaa39a503752caf380f239de53c16b720
KwonSanta <KwonSanta@gmail.com>
2024-05-21 11:17:13 +0900

    /, /write 경로와 Home, BoardWrite 컴포넌트 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./page/Home.jsx";
+import { BoardWrite } from "./page/board/BoardWrite.jsx";
 
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />, // /write request 시 <Home /> 이 보임
+      children: [{ path: "write", element: <BoardWrite /> }], // <Outlet />
+    },
+  ]);
+  return (
+    <ChakraProvider>
+      <RouterProvider router={router} />
+    </ChakraProvider>
+  );
 }
 
 export default App;
