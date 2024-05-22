a6ee0a4b7066fa2c0efa52e3edd5038f3c029503
KwonSanta <KwonSanta@gmail.com>
2024-05-22 09:33:12 +0900

    Member Create :
    회원가입 페이지
    - 가입 버튼 클릭시 post "/api/member/signup" 전송
    - then, catch, finally 작성
    [작업 완료]

--- src/page/member/MemberSignUp.jsx
+++ src/page/member/MemberSignUp.jsx
@@ -1,6 +1,57 @@
-import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  useToast,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function MemberSignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickName, setNickName] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
+
+  function handleClick() {
+    setIsLoading(true);
+    axios
+      .post("/api/member/signup", { email, password, nickName })
+      .then((res) => {
+        toast({
+          status: "success",
+          description: "회원 가입이 완료되었습니다.",
+          position: "top",
+        });
+        // todo : 로그인 화면으로 이동
+        navigate("/");
+      })
+      .catch((err) => {
+        if (err.response.status === 400) {
+          // 400 Code : Client 쪽 오류
+          toast({
+            status: "error",
+            description: "입력값을 확인해 주세요.",
+            position: "top",
+          });
+        } else {
+          toast({
+            status: "error",
+            description: "회원 가입 중 문제가 발생하였습니다.",
+            position: "top",
+          });
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
   return (
     <Box>
       <Box>회원가입</Box>
@@ -20,11 +71,17 @@ export function MemberSignUp() {
         <Box>
           <FormControl>
             <FormLabel>별명</FormLabel>
-            <Input onChange={(e) => set(e.target.value)} />
+            <Input onChange={(e) => setNickName(e.target.value)} />
           </FormControl>
         </Box>
         <Box>
-          <Button colorScheme={"blue"}>가입</Button>
+          <Button
+            isLoading={isLoading}
+            onClick={handleClick}
+            colorScheme={"blue"}
+          >
+            가입
+          </Button>
         </Box>
       </Box>
     </Box>
