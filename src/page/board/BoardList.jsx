139ed940c073294196f64a4292fd6e6e2bd51bc3
KwonSanta <KwonSanta@gmail.com>
2024-05-27 09:27:13 +0900

    게시물 목록에서 페이지 번호 넘기기 useSearchParams

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -1,16 +1,19 @@
 import { Box, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState([]);
   const navigate = useNavigate();
-  //
+  const [searchParams] = useSearchParams();
+
   useEffect(() => {
-    axios.get("/api/board/list").then((res) => setBoardList(res.data));
+    axios
+      .get(`/api/board/list?${searchParams}`)
+      .then((res) => setBoardList(res.data));
   }, []);
 
   return (
@@ -21,7 +24,7 @@ export function BoardList() {
           <Thead>
             <Tr>
               <Th>#</Th>
-              <Th>Title</Th>
+              <Th>TITLE</Th>
               <Th>
                 <FontAwesomeIcon icon={faUserPen} />
               </Th>
