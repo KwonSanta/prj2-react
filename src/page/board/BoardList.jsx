15138a14f6101022dd28206d199b0ec305e3cc73
KwonSanta <KwonSanta@gmail.com>
2024-05-30 15:14:15 +0900

    게시물 목록에 댓글 수 표시

--- src/page/board/BoardList.jsx
+++ src/page/board/BoardList.jsx
@@ -19,6 +19,7 @@ import {
   faAngleRight,
   faAnglesLeft,
   faAnglesRight,
+  faComments,
   faHeart,
   faImages,
   faMagnifyingGlass,
@@ -104,6 +105,12 @@ export function BoardList() {
                       <Badge>
                         <FontAwesomeIcon icon={faImages} />
                         {board.numberOfImages}
+                      </Badge>
+                    )}
+                    {board.numberOfComments > 0 && (
+                      <Badge>
+                        <FontAwesomeIcon icon={faComments} />
+                        {board.numberOfComments}
                       </Badge>
                     )}
                   </Td>
